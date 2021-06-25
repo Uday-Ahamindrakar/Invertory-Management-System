b142e3db2e7df02a895065a3224677e743644234
Uday Sanjay Ahamindrakar <udayahamindrakar4441@gmail.com>
2021-06-25 16:24:20 +0530

    First Build

---
+++ src/pos/DBconnection.java
@@ -0,0 +1,27 @@
+package pos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection 
+{
+	
+	Connection con;
+	
+	public static Connection DBconnect()
+	{
+		DBconnection connect = new DBconnection();
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			connect.con = DriverManager.getConnection("jdbc:mysql://localhost:3308/javapos", "root", "");
+			return connect.con;
+		} catch (Exception e)
+		{
+			
+			System.out.println("Failure"+e);
+			return null;
+		}
+	}
+}
